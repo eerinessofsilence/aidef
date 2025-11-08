a513477dbc156f9f3b9df86dc5f6f4492329074a
eerinessofsilence <eerinessbusiness@gmail.com>
2025-11-08 16:27:46 +0200

    feat(frontend): add template home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,25 @@
+import Hero from "../../components/hero";
+import MissionSection from "../../components/mission-section";
+import FocusAreasSection from "../../components/focus-areas-section";
+import TeamSection from "../../components/team-section";
+import VideoSection from "../../components/video-section";
+import DroneVariant from "../../components/drone-variant";
+import DroneGrid from "../../components/drone-grid";
+import GallerySection from "../../components/gallery-section";
+import Footer from "../../components/footer";
+
 export default function Home() {
-  return <main className="bg-background min-h-screen"></main>;
+  return (
+    <main className="bg-background min-h-screen">
+      <Hero />
+      <MissionSection />
+      <DroneGrid />
+      <GallerySection />
+      <FocusAreasSection />
+      <VideoSection />
+      <TeamSection />
+      <DroneVariant />
+      <Footer />
+    </main>
+  );
 }
