0614d495a9dfbd51e169a42e7f548975cff9e869
eerinessofsilence <eerinessbusiness@gmail.com>
2025-11-11 18:30:58 +0200

    refactor(frontend): restyle header

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,79 +1,132 @@
 "use client";
-
-import { Menu } from "lucide-react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "../components/ui/sheet";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ChevronDown } from "lucide-react";
 
 const NAV_LINKS = [
-  { href: "/", label: "Home" },
-  { href: "/products", label: "Products" },
-  { href: "#support", label: "Support" },
+  {
+    link: "/",
+    text: "Home",
+  },
+  {
+    link: "/products",
+    text: "Products",
+  },
+  {
+    link: "/services",
+    text: "Services",
+  },
+  {
+    link: "#footer",
+    text: "Support",
+  },
 ];
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const isOpen = open === true;
+
   return (
-    <header className="border-border bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
-      <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4">
-        <a href="/" className="flex items-center space-x-2">
-          <img width={100} src="./logo aidef.svg" alt="" />
-        </a>
-
-        <div className="hidden gap-12 md:flex">
-          {NAV_LINKS.map(({ href, label }) => (
-            <a
-              href={href}
-              className="text-foreground hover:text-foreground/50 font-medium uppercase transition-colors duration-300"
-            >
-              {label}
-            </a>
-          ))}
-        </div>
-
-        <div className="flex items-center gap-3">
-          <a href="#footer">
-            <button className="tracking-wide-caps text-background hover:text-foreground hover:bg-background bg-foreground hidden h-10 w-36 items-center justify-center rounded-xl uppercase transition-all duration-300 ease-in-out md:inline-flex">
-              Contact Us
-            </button>
+    <div className="container mx-auto py-8">
+      <header className="border-border z-50 rounded-3xl border bg-black/15 px-8 py-6 shadow-sm shadow-black backdrop-blur-md">
+        <div className="flex items-center justify-between">
+          <a href="/" className="flex items-center space-x-2">
+            <img src="./logo aidef.svg" className="w-34" alt="" />
           </a>
 
-          <Sheet>
-            <SheetTrigger asChild>
-              <button className="hover:bg-foreground/25 border-secondary bg-foreground/75 text-foreground flex transform-gpu items-center justify-center rounded-xl border p-2 transition-all duration-300 ease-out hover:-translate-y-0.5 active:translate-y-px motion-reduce:transform-none md:hidden">
-                <Menu className="size-8 text-black" />
-                <span className="sr-only">Open menu</span>
-              </button>
-            </SheetTrigger>
-            <SheetContent
-              side="right"
-              className="flex h-full flex-col gap-8 px-6 py-10"
-            >
-              <nav className="flex flex-col gap-6">
-                {NAV_LINKS.map(({ href, label }) => (
-                  <SheetClose className="size" asChild key={href}>
-                    <a
-                      href={href}
-                      className="tracking-wide-caps text-foreground hover:text-foreground/50 font-medium uppercase transition-colors duration-300"
-                    >
-                      {label}
-                    </a>
-                  </SheetClose>
-                ))}
-              </nav>
-              <SheetClose asChild>
-                <a href="#footer" className="mt-auto">
-                  <button className="tracking-wide-caps text-background hover:text-foreground hover:bg-background bg-foreground mt-auto h-10 w-full rounded-xl uppercase transition-all duration-300 ease-in-out">
-                    Contact Us
+          <div className="flex items-center gap-6">
+            {NAV_LINKS.map(({ link, text }) => {
+              const isProducts = text === "Products";
+              if (!isProducts) {
+                return (
+                  <Link
+                    key={link}
+                    to={link}
+                    className="text-foreground hover:text-foreground/50 flex items-center text-lg font-medium transition-colors duration-300"
+                  >
+                    {text}
+                  </Link>
+                );
+              } else {
+                return (
+                  <button
+                    key={link}
+                    onClick={() => setOpen(isOpen ? false : true)}
+                    className="text-foreground hover:text-foreground/50 flex items-center font-medium transition-colors duration-300"
+                  >
+                    {text}
+                    <ChevronDown
+                      className={`h-5 w-5 transition-transform duration-300 ${
+                        isOpen ? "rotate-180" : ""
+                      }`}
+                    />
                   </button>
-                </a>
-              </SheetClose>
-            </SheetContent>
-          </Sheet>
+                );
+              }
+            })}
+          </div>
+
+          <div className="flex items-center gap-6">
+            <a
+              href="#"
+              className="border-border w-10 rounded-xl border bg-white/25 p-2 shadow-lg shadow-black/25 backdrop-blur-2xl"
+            >
+              <img src="./placeholder.svg" alt="" />
+            </a>
+            <a
+              href="#"
+              className="border-border w-10 rounded-xl border bg-white/25 p-2 shadow-lg shadow-black/25 backdrop-blur-2xl"
+            >
+              <img src="./placeholder.svg" alt="" />
+            </a>
+            <a
+              href="#"
+              className="border-border w-10 rounded-xl border bg-white/25 p-2 shadow-lg shadow-black/25 backdrop-blur-2xl"
+            >
+              <img src="./placeholder.svg" alt="" />
+            </a>
+            <a href="#footer">
+              <button className="text-background hover:text-foreground hover:bg-background bg-foreground hidden h-11 w-36 items-center justify-center rounded-xl font-bold uppercase transition-all duration-300 ease-in-out md:inline-flex">
+                Contact Us
+              </button>
+            </a>
+          </div>
+        </div>
+      </header>
+      <div className="absolute top-32 right-[10%] z-50">
+        <div
+          className={`border-border mt-3 grid w-fit grid-cols-2 gap-x-12 gap-y-6 rounded-3xl border bg-black/25 p-8 opacity-0 shadow-lg shadow-black/25 backdrop-blur-md transition-all duration-300 ${isOpen ? "opacity-100" : ""}`}
+        >
+          <div className="flex items-center gap-6">
+            <div className="border-border w-16 rounded-2xl border bg-white/25 p-3 shadow-md shadow-black/25 backdrop-blur-2xl">
+              <img src="./placeholder.svg" alt="" />
+            </div>
+            <h1 className="text-xl font-bold">
+              Autonomous unmanned aerial vehicles
+            </h1>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="border-border w-16 rounded-2xl border bg-white/25 p-3 shadow-md shadow-black/25 backdrop-blur-2xl">
+              <img src="./placeholder.svg" alt="" />
+            </div>
+            <h1 className="text-xl font-bold">
+              Components for UAV manufacturing
+            </h1>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="border-border w-16 rounded-2xl border bg-white/25 p-3 shadow-md shadow-black/25 backdrop-blur-2xl">
+              <img src="./placeholder.svg" alt="" />
+            </div>
+            <h1 className="text-xl font-bold">Unmanned ground vehicle</h1>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="border-border w-16 rounded-2xl border bg-white/25 p-3 shadow-md shadow-black/25 backdrop-blur-2xl">
+              <img src="./placeholder.svg" alt="" />
+            </div>
+            <h1 className="text-xl font-bold">Small ARMS</h1>
+          </div>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
