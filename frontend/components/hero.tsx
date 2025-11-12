2ba4dc20296a9f53f6dc7b513258dd784202b0b0
eerinessofsilence <eerinessbusiness@gmail.com>
2025-11-12 13:49:11 +0200

    refactor(frontend): restyle hero section

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -1,31 +1,33 @@
 export default function Hero() {
   return (
-    <section className="container mx-auto py-32">
-      <div className="space-y-8">
-        <div>
-          <h1 className="text-text text-[84px] leading-26 font-bold uppercase">
-            High-speed <br /> dual-mode UAV
-          </h1>
-        </div>
-        <div className="flex w-fit flex-col space-y-8">
-          <p className="text-lg leading-8 uppercase">
-            Ground-to-ground and ground-to-air precision <br /> strikes with HE
-            and HEF warheads
-          </p>
-          <div className="space-x-4">
-            <a href="#">
-              <button className="text-text-alt rounded-lg bg-white px-6 py-4 text-xl font-bold uppercase">
-                Our products
-              </button>
-            </a>
-            <a href="#">
-              <button className="text-text rounded-lg border border-white bg-black/5 px-6 py-4 text-xl font-bold uppercase">
-                Contact us
-              </button>
-            </a>
+    <div className="aspect-1442/772 bg-[url(./hero-bg.png),linear-gradient(rgba(50,150,255,0.1))] bg-cover bg-center bg-no-repeat py-67.5">
+      <section className="container m-auto">
+        <div className="space-y-8">
+          <div>
+            <h1 className="text-text text-[84px] leading-26 font-bold uppercase">
+              High-speed <br /> dual-mode UAV
+            </h1>
+          </div>
+          <div className="flex w-fit flex-col space-y-10">
+            <p className="text-lg leading-8 uppercase">
+              Ground-to-ground and ground-to-air precision <br /> strikes with
+              HE and HEF warheads
+            </p>
+            <div className="space-x-4">
+              <a href="#">
+                <button className="text-text-alt rounded-lg bg-white px-6 py-4 text-xl font-bold uppercase">
+                  Our products
+                </button>
+              </a>
+              <a href="#">
+                <button className="text-text rounded-lg border border-white bg-black/5 px-6 py-4 text-xl font-bold uppercase">
+                  Contact us
+                </button>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
