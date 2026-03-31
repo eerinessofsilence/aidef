ec1d4f4a530401e3c7d523bacd81483a83d8751f
eerinessofsilence <eerinessbusiness@gmail.com>
2026-03-31 14:34:08 +0300

    fix(frontend): fix loading content under hero block in home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,19 @@
-import { lazy, Suspense } from "react";
 import Hero from "../../components/home/Hero";
-import { useInViewOnce } from "../../hooks/use-in-view-once";
-
-const DroneCarousel = lazy(() => import("../../components/home/DroneCarousel"));
-const FocusAreas = lazy(() => import("../../components/home/FocusAreas"));
-const SystemIntegration = lazy(
-  () => import("../../components/home/SystemIntegration"),
-);
-const CustomerBenefits = lazy(
-  () => import("../../components/home/CustomerBenefits"),
-);
-const BlogPosts = lazy(() => import("../../components/home/BlogPosts"));
-
-function HomeSectionsFallback() {
-  return <div className="min-h-[120vh]" aria-hidden="true" />;
-}
+import DroneCarousel from "../../components/home/DroneCarousel";
+import FocusAreas from "../../components/home/FocusAreas";
+import SystemIntegration from "../../components/home/SystemIntegration";
+import CustomerBenefits from "../../components/home/CustomerBenefits";
+import BlogPosts from "../../components/home/BlogPosts";
 
 export default function Home() {
-  const { ref: deferredSectionsRef, inView: showDeferredSections } =
-    useInViewOnce({
-      rootMargin: "0px 0px -35% 0px",
-    });
-
   return (
     <main className="relative min-h-screen">
       <Hero />
-      <div ref={deferredSectionsRef} className="h-px w-full" aria-hidden="true" />
-      {showDeferredSections ? (
-        <Suspense fallback={<HomeSectionsFallback />}>
-          <DroneCarousel />
-          <FocusAreas />
-          <SystemIntegration />
-          <BlogPosts />
-          <CustomerBenefits />
-        </Suspense>
-      ) : (
-        <HomeSectionsFallback />
-      )}
+      <DroneCarousel />
+      <FocusAreas />
+      <SystemIntegration />
+      <BlogPosts />
+      <CustomerBenefits />
     </main>
   );
 }
